7f24f6b031d033a48391f87e17d2a09eab4429a4
OfficialChrisHusky <81988470+OfficialChrisHusky@users.noreply.github.com>
2022-06-08 19:52:27 +0200

    Player Movement

--- Element Survival/Assets/Scripts/Player/PlayerLook.cs
+++ Element Survival/Assets/Scripts/Player/PlayerLook.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLook : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerLook : MonoBehaviour {
+
+    [SerializeField] private float sensitivity;
+    [SerializeField] private Transform body;
+
+    float xRot;
+    float yRot;
+
+    private void Start() {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+
+        yRot += Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * sensitivity;
+        xRot -= Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * sensitivity;
+
+        xRot = Mathf.Clamp(xRot, -90.0f, 90.0f);
+
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0.0f);
+        body.rotation = Quaternion.Euler(0.0f, yRot, 0.0f);
+
     }
+
 }
--- Element Survival/Assets/Scripts/Player/PlayerMovement.cs
+++ Element Survival/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,17 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+
+    [Header("Sprinting")]
+    [SerializeField] private float sprintMultiplier = 1.5f;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpHeight;
+
+    [Header("Ground Detection")]
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+
+    [Header("Drag Control")]
+    [SerializeField] private float groundDrag = 1.0f;
+    [SerializeField] private float airDrag = 0.5f;
+
+    Rigidbody rb;
+    Vector3 direction;
+    float multiplier = 1.0f;
+
+    private void Start() {
         
+        rb = gameObject.GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical   = Input.GetAxisRaw("Vertical");
+
+        direction = transform.forward * vertical + transform.right * horizontal;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded) { rb.drag = groundDrag; multiplier = 1.0f; } else { rb.drag = airDrag; multiplier = 0.3f; }
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded) { multiplier = sprintMultiplier; }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+
+            rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+
+        }
+
     }
+
+    private void FixedUpdate() {
+        
+        rb.AddForce(direction.normalized * moveSpeed * 100.0f * Time.deltaTime * multiplier, ForceMode.Acceleration);
+
+    }
+
 }
