a94121eed354ed3cb6559f6fd27b682ea0ed850b
Kai Williams-Price <107143016+khaios11@users.noreply.github.com>
2022-07-31 03:01:44 +0100

    settings

---
+++ Element Survival/Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+
+    public void Start ()
+    {
+        
+    }
+
+
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void ToggleFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+   
+}
