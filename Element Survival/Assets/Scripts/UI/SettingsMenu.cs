4001dea63480261007a4e64553036cf24eaa0a87
Kai Williams-Price <107143016+khaios11@users.noreply.github.com>
2022-08-15 20:51:36 +0100

    Resolutions + vSync

--- Element Survival/Assets/Scripts/UI/SettingsMenu.cs
+++ Element Survival/Assets/Scripts/UI/SettingsMenu.cs
@@ -7,15 +7,43 @@ using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public Toggle fullscreenTog, vsyncTog;
+
     public AudioMixer audioMixer;
 
-
+    public List<Resolutions> resolutions = new List<Resolutions>();
+    private int SelectedResolution;
+    public TMP_Text ResLabel;
 
     public void Start ()
     {
-        
+        fullscreenTog.isOn = Screen.fullScreen;
+
+        if (QualitySettings.vSyncCount == 0)
+        {
+            vsyncTog.isOn = false;
+        }
+        else
+        {
+            vsyncTog.isOn = true;
+        }
     }
 
+    public void ApplyGraphics()
+    {
+        //Screen.fullScreen = fullscreenTog.isOn;
+
+            if (vsyncTog.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        Screen.SetResolution(resolutions[SelectedResolution].HorizontalR, resolutions[SelectedResolution].VerticalR, Screen.fullScreen = fullscreenTog.isOn);
+    }
 
     public void SetVolume (float volume)
     {
@@ -23,15 +51,46 @@ public class SettingsMenu : MonoBehaviour
         audioMixer.SetFloat("MasterVolume", volume);
     }
 
-    public void ToggleFullscreen (bool isFullScreen)
-    {
-        Screen.fullScreen = isFullScreen;
-    }
+    //public void ToggleFullscreen (bool isFullScreen)
+    //{
+    //    Screen.fullScreen = isFullScreen;
+    //}
 
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
-   
+    public void ResLeft()
+    {
+        SelectedResolution--;
+        if (SelectedResolution < 0)
+        {
+            SelectedResolution = 0;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        SelectedResolution++;
+        if (SelectedResolution > resolutions.Count - 1)
+        {
+            SelectedResolution = resolutions.Count - 1;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel ()
+    {
+        ResLabel.text = resolutions[SelectedResolution].HorizontalR.ToString() + " x " + resolutions[SelectedResolution].VerticalR.ToString();
+    }
+}
+
+[System.Serializable]
+public class Resolutions 
+{
+    public int HorizontalR, VerticalR;
 }
